8583a3a3a661698a7e64279f60cc5991d160e2f8
Stefan Rimaila <stefan@rimaila.fi>
2023-08-10 06:58:41 +0300

    add countdown for reasons

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,29 @@
-import { Manrope } from '@next/font/google';
+import { Manrope, Space_Mono } from '@next/font/google';
 import 'normalize.css';
 
 import '@/styles/globals.css';
 
 const manrope = Manrope({ subsets: ['latin'] });
+const spacemono = Space_Mono({ subsets: ['latin'], weight: '400' });
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <style jsx global>
         {`
+          html,
+          body,
+          #__next {
+            height: 100%;
+          }
+
           html {
             font-family: ${manrope.style.fontFamily};
+          }
+
+          pre,
+          code {
+            font-family: ${spacemono.style.fontFamily};
           }
         `}
       </style>
---
+++ src/pages/lab/countdown/index.js
@@ -0,0 +1,36 @@
+import clsx from 'clsx';
+import { useEffect, useMemo, useState } from 'react';
+import { intervalToDuration, formatDuration } from 'date-fns';
+
+import css from '@/styles/lab/Countdown.module.css';
+
+export default function CountdownPage() {
+  const [state, setState] = useState({
+    start: new Date(),
+    end: new Date(2023, 7, 24, 6, 40, 0),
+  });
+
+  const dur = useMemo(() => intervalToDuration(state), [state]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setState(s => ({ ...s, start: new Date() }));
+    }, 500);
+
+    return () => {
+      clearInterval(interval);
+    };
+  });
+
+  const s = [dur.days, dur.hours, dur.minutes, dur.seconds].map(x =>
+    `${x}`.padStart(2, '0'),
+  );
+
+  return (
+    <div className={clsx(css.body)}>
+      <div className={clsx(css.display)}>
+        <code>{s.join(':')}</code>
+      </div>
+    </div>
+  );
+}
